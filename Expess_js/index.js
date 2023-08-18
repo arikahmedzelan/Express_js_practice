3cf210d7814174a692299a11ee8322dd5bfffea2
Arik Ahmed Zelan <info.zelankathi2001@gmail.com>
2023-08-19 02:52:02 +0600

    Routing

--- Expess_js/index.js
+++ Expess_js/index.js
@@ -1,14 +1,51 @@
 
+
+//1
+// var exprss=require('express');
+//
+// app=exprss();
+//
+// app.get("/",function (req,res) {
+//
+//     res.send("Hello Express js");
+// });
+// app.listen(8000,function () {
+//
+//     console.log("Server run success")
+//
+// })
+
+//2 routing
 var exprss=require('express');
 
 app=exprss();
 
 app.get("/",function (req,res) {
 
-    res.send("Hello Express js");
+    res.send("Home Page");
 });
+
+
+app.post("/About",function (req,res) {
+
+    res.send("About page");
+});
+
+
+app.put("/contact",function (req,res) {
+
+    res.send("Contact Page");
+});
+
+app.delete("/terms",function (req,res) {
+
+    res.send("Terms Page");
+});
+
+
 app.listen(8000,function () {
 
     console.log("Server run success")
 
 })
+
